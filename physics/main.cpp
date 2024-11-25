898f52f6f0df9503086672adb8155c51d8ce43a5
R.Reid <40534638@live.napier.ac.uk>
2024-11-25 18:49:12 +0000

    physics done

--- physics/main.cpp
+++ physics/main.cpp
@@ -1,170 +1,166 @@
+//main.cpp
 #include <SFML/Graphics.hpp>
+#include <Box2d/Box2d.h>
 
 using namespace sf;
-using namespace std;
 
-const Keyboard::Key controls[4] = {
-    Keyboard::A,   // Player1 UP
-    Keyboard::Z,   // Player1 Down
-    Keyboard::Up,  // Player2 UP
-    Keyboard::Down // Player2 Down
-};
-const Vector2f paddleSize(25.f, 100.f);
-const float ballRadius = 10.f;
-const int gameWidth = 800;
-const int gameHeight = 600;
-const float paddleSpeed = 400.f;
-const float paddleOffsetWall = 10.f;
+b2World* world;
+const int gameWidth = 1920;
+const int gameHeight = 1080;
 
-Vector2f ballVelocity;
-bool isPlayer1Serving = false;
-const float initialVelocityX = 100.f;
-const float initialVelocityY = 60.f;
-const float velocityMultiplier = 1.1f;
+std::vector<b2Body*> bodies;
+std::vector<RectangleShape*> sprites;
 
-CircleShape ball;
-RectangleShape paddles[2];
 
-void Reset();
+// 1 sfml unit = 30 physics units
+const float physics_scale = 30.0f;
+// inverse of physics_scale, useful for calculations
+const float physics_scale_inv = 1.0f / physics_scale;
+// Magic numbers for accuracy of physics simulation
+const int32 velocityIterations = 6;
+const int32 positionIterations = 2;
 
-void Load() {
-    // Set size and origin of paddles
-    for (auto& p : paddles) {
-        p.setSize(paddleSize);
-        p.setOrigin(paddleSize / 2.f);
-    }
-    // Set size and origin of ball
-    ball.setRadius(ballRadius);
-    ball.setOrigin(ballRadius/2.f, ballRadius/2.f); //Should be half the ball width and height
-
-    // Set Initial Positions
-    Reset();
+//Convert from b2Vec2 to a Vector2f
+inline const Vector2f bv2_to_sv2(const b2Vec2& in) {
+	return Vector2f(in.x * physics_scale, (in.y * physics_scale));
+}
+//Convert from Vector2f to a b2Vec2
+inline const b2Vec2 sv2_to_bv2(const Vector2f& in) {
+	return b2Vec2(in.x * physics_scale_inv, (in.y * physics_scale_inv));
+}
+//Convert from screenspace.y to physics.y (as they are the other way around)
+inline const Vector2f invert_height(const Vector2f& in) {
+	return Vector2f(in.x, gameHeight - in.y);
 }
 
-void Update(RenderWindow& window) {
-    // Reset clock, recalculate deltatime
-    static Clock clock;
-    float dt = clock.restart().asSeconds();
-    // check and consume events
-    Event event;
-    while (window.pollEvent(event)) {
-        if (event.type == Event::Closed) {
-            window.close();
-            return;
-        }
-    }
+//Create a Box2D body with a box fixture
+b2Body* CreatePhysicsBox(b2World& World, const bool dynamic, const Vector2f& position, const Vector2f& size) {
+	b2BodyDef BodyDef;
+	//Is Dynamic(moving), or static(Stationary)
+	BodyDef.type = dynamic ? b2_dynamicBody : b2_staticBody;
+	BodyDef.position = sv2_to_bv2(position);
+	//Create the body
+	b2Body* body = World.CreateBody(&BodyDef);
 
-    // Quit Via ESC Key
-    if (Keyboard::isKeyPressed(Keyboard::Escape)) {
-        window.close();
-    }
-
-    // handle player 1 paddle movement
-    float direction = 0.0f;
-    if (Keyboard::isKeyPressed(controls[0])) {
-        direction--;
-    }
-    if (Keyboard::isKeyPressed(controls[1])) {
-        direction++;
-    }
-    paddles[0].move(Vector2f(0.f, direction * paddleSpeed * dt));
-
-    // handle player 2 paddle movement
-    direction = 0.0f;
-    if (Keyboard::isKeyPressed(controls[2])) {
-        direction--;
-    }
-    if (Keyboard::isKeyPressed(controls[3])) {
-        direction++;
-    }
-
-    // Check for edge collision
-    Vector2f moveVec = Vector2f(0.f, direction * paddleSpeed * dt);
-    Vector2f newPos = (paddles[1].getPosition() + moveVec);
-
-    if (!(newPos.y + paddleSize.y / 2.f > gameHeight || newPos.y - paddleSize.y / 2.f < 0)) {
-        paddles[1].move(moveVec);
-    }
-
-    // ball movement
-    ball.move(ballVelocity * dt);
-
-    // check ball collision
-    const float bx = ball.getPosition().x;
-    const float by = ball.getPosition().y;
-    if (by > gameHeight) { //bottom wall
-        // bottom wall
-        ballVelocity.x *= velocityMultiplier;
-        ballVelocity.y *= -velocityMultiplier;
-        ball.move(Vector2f(0.f, -10.f));
-    }
-    else if (by < 0) { //top wall
-        // top wall
-        ballVelocity.x *= velocityMultiplier;
-        ballVelocity.y *= -velocityMultiplier;
-        ball.move(Vector2f(0.f, 10.f));
-    }
-    else if (bx > gameWidth) {
-        // right wall
-        Reset();
-    }
-    else if (bx < 0) {
-        // left wall
-        Reset();
-    }
-    else if (
-        //ball is inline or behind paddle AND
-        bx > gameWidth - paddleSize.x - paddleOffsetWall &&
-        //ball is below top edge of paddle AND
-        by > paddles[1].getPosition().y - (paddleSize.y * 0.5) &&
-        //ball is above bottom edge of paddle
-        by < paddles[1].getPosition().y + (paddleSize.y * 0.5))
-    {
-        // bounce off right paddle
-        ballVelocity.x *= -velocityMultiplier;
-        ballVelocity.y *= velocityMultiplier;
-
-    }
-    else if (
-        //ball is inline or behind paddle AND
-        bx < paddleSize.x + paddleOffsetWall &&
-        //ball is below top edge of paddle AND
-        by > paddles[0].getPosition().y - (paddleSize.y * 0.5) &&
-        //ball is above bottom edge of paddle
-        by < paddles[0].getPosition().y + (paddleSize.y * 0.5)) {
-
-            // bounce off left paddle
-            ballVelocity.x *= -velocityMultiplier;
-            ballVelocity.y *= velocityMultiplier;
-    }
+	//Create the fixture shape
+	b2PolygonShape Shape;
+	Shape.SetAsBox(sv2_to_bv2(size).x * 0.5f, sv2_to_bv2(size).y * 0.5f);
+	b2FixtureDef FixtureDef;
+	//Fixture properties
+	FixtureDef.density = dynamic ? 10.f : 0.f;
+	FixtureDef.friction = dynamic ? 0.8f : 1.f;
+	FixtureDef.restitution = 1.0;
+	FixtureDef.shape = &Shape;
+	//Add to body
+	body->CreateFixture(&FixtureDef);
+	return body;
 }
 
-void Reset() {
-
-    // reset paddle position
-    paddles[0].setPosition(Vector2f(paddleOffsetWall + paddleSize.x / 2.f, gameHeight / 2.f));
-    paddles[1].setPosition(Vector2f(gameWidth - (paddleOffsetWall + paddleSize.x / 2.f), gameHeight / 2.f));
-    // reset Ball Position
-    ball.setPosition(Vector2f(gameWidth / 2.f, gameHeight / 2.f));
-    // Set initial ball velocity
-    ballVelocity = { (isPlayer1Serving ? initialVelocityX : -initialVelocityX), initialVelocityY };
+// Create a Box2d body with a box fixture, from a sfml::RectangleShape
+b2Body* CreatePhysicsBox(b2World& world, const bool dynamic, const RectangleShape& rs) {
+	return CreatePhysicsBox(world, dynamic, rs.getPosition(), rs.getSize());
 }
 
-void Render(RenderWindow& window) {
-    // Draw Everything
-    window.draw(paddles[0]);
-    window.draw(paddles[1]);
-    window.draw(ball);
+
+void init() {
+	const b2Vec2 gravity(0.0f, -10.0f);
+
+	// Construct a world, which holds and simulates the physics bodies.
+	world = new b2World(gravity);
+
+	// Wall Dimensions
+	Vector2f walls[] = {
+		// Top
+		Vector2f(gameWidth * .5f, 5.f), Vector2f(gameWidth, 10.f),
+		// Bottom
+		Vector2f(gameWidth * .5f, gameHeight - 5.f), Vector2f(gameWidth, 10.f),
+		// left
+		Vector2f(5.f, gameHeight * .5f), Vector2f(10.f, gameHeight),
+		// right
+		Vector2f(gameWidth - 5.f, gameHeight * .5f), Vector2f(10.f, gameHeight)
+	};
+
+	for (int i = 0; i < 7; i += 2) {
+		// Create SFML shapes for each wall
+		auto s = new RectangleShape();
+		s->setPosition({ walls[i].x - 10.f, walls[i].y - 10.f });
+		s->setSize(walls[i+1]);
+		s->setOrigin(walls[i]);
+		s->setFillColor(Color::White);
+		sprites.push_back(s);
+
+		// Create a static physics body for the wall
+		auto b = CreatePhysicsBox(*world, false, *s);
+		// Give the box a spin
+		b->ApplyAngularImpulse(5.0f, true);
+		bodies.push_back(b);
+	}
+
+	// Create Boxes
+	for (int i = 1; i < 11; ++i) {
+		// Create SFML shapes for each box
+		auto s = new RectangleShape();
+		s->setPosition(Vector2f(i * (gameWidth / 12.f), gameHeight * .7f));
+		s->setSize(Vector2f(50.0f, 50.0f));
+		s->setOrigin(Vector2f(25.0f, 25.0f));
+		s->setFillColor(Color::White);
+		sprites.push_back(s);
+
+		// Create a dynamic physics body for the box
+		auto b = CreatePhysicsBox(*world, true, *s);
+		// Give the box a spin
+		b->ApplyAngularImpulse(5.0f, true);
+		bodies.push_back(b);
+	}
+
+
 }
+
+void Update() {
+	static sf::Clock clock;
+	float dt = clock.restart().asSeconds();
+
+	// Step Physics world by dt (non-fixed timestep) - THIS DOES ALL THE ACTUAL SIMULATION, DON'T FORGET THIS!
+	world->Step(dt, velocityIterations, positionIterations);
+
+	for (int i = 0; i < bodies.size(); ++i) {
+		// Sync Sprites to physics position
+		sprites[i]->setPosition(invert_height(bv2_to_sv2(bodies[i]->GetPosition())));
+		// Sync Sprites to physics Rotation
+		sprites[i]->setRotation((180 / b2_pi) * bodies[i]->GetAngle());
+	}
+}
+
 
 int main() {
-    RenderWindow window(VideoMode(gameWidth, gameHeight), "PONG");
-    Load();
-    while (window.isOpen()) {
-        window.clear();
-        Update(window);
-        Render(window);
-        window.display();
-    }
-    return 0;
+	RenderWindow window(VideoMode(gameWidth, gameHeight), "Physics");
+	init();
+	while (window.isOpen()) {
+
+		// check and consume events
+		Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == Event::Closed) {
+				window.close();
+				break;
+			}
+		}
+
+		// Quit Via ESC Key
+		if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+			window.close();
+		}
+
+		window.clear();
+
+		//call invaders render function
+		for (const auto s : sprites) {
+			window.draw(*s);
+		}
+
+		Update();
+		window.display();
+	}
+	return 0;
+
 }
